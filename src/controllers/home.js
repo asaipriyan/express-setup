9e5830e83112a1c8798ebfb939ec99da2ebddb42
Asaipriyan <asaipriyan.s@centative.com>
2021-01-29 14:59:50 +0530

    express controller and pg setup are completed

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-useless-constructor */
-/* eslint-disable padded-blocks */
-/* eslint-disable no-empty-function */
 import { testEnvironmentVariable } from '../setting';
 
 export const indexPage = (_req, res) => res.status(200).json({ message: testEnvironmentVariable });
